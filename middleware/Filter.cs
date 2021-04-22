8a76932f0cfeacd54ccbe726c8c7ef8008a35c3e
Ridek240 <64654063+Ridek240@users.noreply.github.com>
2021-04-22 13:06:41 +0200

    Dodaj pliki projektów.

---
+++ middleware/Filter.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using BrowserNET;
+using Microsoft.AspNetCore.Http;
+
+namespace zad6.middleware
+{
+    public class Filter
+    {
+        private RequestDelegate _next;
+        public Filter(RequestDelegate next)
+        {
+            _next = next;
+        }
+
+        public async Task Invoke(HttpContext context)
+        {
+            var parser = new UserAgentParser(context.Request.Headers["User-Agent"]);
+            parser.Determine();
+
+            var browserName = parser.BrowserName;
+            var forbittenBrowsers = new List<string> { "Edge", "Internet Explorer", "EdgeChromium" };
+
+            if (forbittenBrowsers.Any(browserName.Contains))
+                await context.Response.WriteAsync("Nielegalnia przeglądarka");
+            else
+                await _next(context);
+        }
+    }
+}
---
+++ middleware/Filtere.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+
+namespace zad6.middleware
+{
+    public static class Filtere
+    {
+        public static IApplicationBuilder UseFilter(this IApplicationBuilder builder)
+        {
+            return builder.UseMiddleware<Filter>();
+        }
+    }
+}
